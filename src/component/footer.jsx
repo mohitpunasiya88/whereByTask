ed587b3463fd68de202020d712a03889f3746658
mohitpunasiya88 <142258101+mohitpunasiya88@users.noreply.github.com>
2023-08-16 15:54:57 +0530

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,17 +1,72 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { all } from "axios";
 
 function Footer() {
-    const [data,setData] = useState();
+    const [data,setData] = useState([]);
+    const [name,setName] = useState();
+    const [lastName,setLastName] = useState();
+    const [editId,setEditId] = useState("");
+
+    var getUser = async()=>{
+     await axios.get(`http://localhost:3000/getusers`).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+        console.log(response.data)
+      }).catch((err) => {
+        console.log(err)
+      });
+    }
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/users`).then((response) => {
-          setData(response.data);
-          console.log(response.data)
-        }).catch((err) => {
-          console.log(err)
-        });
+      getUser();
       }, []);
+
+
+    const addUser = () => {
+      var alldata = {
+        name: name,
+        lastname : lastName
+      }
+      {editId ? 
+        axios.put(`http://localhost:3000/edituser/${editId}`,alldata).then((response) => {
+        console.log(response.data)
+        getUser();
+        setName("")
+        setLastName("")
+        setEditId("")
+
+      }).catch((err) => {
+        console.log(err)
+      })
+         :
+      axios.post(`http://localhost:3000/postusers`,alldata).then((response) => {
+        console.log(response.data)
+        getUser();
+        setName("")
+        setLastName("")
+      }).catch((err) => {
+        console.log(err)
+      });
+    }
+     
+    }
+
+
+    const deleteClick = async(id) => {debugger
+      await axios.delete(`http://localhost:3000/deleteuser/${id}`).then((response) => {
+        console.log(response.data)
+        getUser();
+      }).catch((err) => {
+        console.log(err)
+      });
+}
+
+
+const editClick = async(id) => {
+  setName(id.name);
+  setLastName(id.lastname);
+  setEditId(id._id)
+}
     return (
         <>
             <div className="footerMain">
@@ -63,21 +118,26 @@ function Footer() {
 
                 </div>
             </div>
-            <table class=" table table-dark table-hover">
+
+            <div className="row">
+              <div className="col-8">
+              <table class=" table table-dark table-hover">
   <thead>
     <tr>
-      <th scope="col">#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
     </tr>
   </thead>
   <tbody>
-    {data?.map((dt)=>
-     <tr>
-     <td>{dt.id}</td>
-     <td>{dt.name}</td>
-     <td>{dt.username}</td>
-   </tr>
+    {data && data?.map(dt=>
+    ( <tr>
+     <td>{dt?.name}</td>
+     <td>{dt?.lastname}</td>
+     <td><button onClick={()=>editClick(dt)} >Edit</button></td>
+     <td><button onClick={()=>deleteClick(dt._id)}>Delete</button></td>
+   </tr>)
     )}
    
    
@@ -86,6 +146,37 @@ function Footer() {
       <td>@twitter</td>
   </tbody>
 </table>
+              </div>
+              <div className="col-4">
+
+
+              <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                Add Users
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group mt-2">
+                            <label for="username">Name</label>
+                            <input value={name} onChange={(e)=>setName(e.target.value)} type="text" class="form-control" id="username" placeholder="Enter Name"/>
+                        </div>
+                        <div class="form-group mt-3" >
+                            <label for="lastname">Last name</label>
+                            <input value={lastName} onChange={(e)=>setLastName(e.target.value)} type="text" class="form-control" id="lastname" placeholder="Lastname"/>
+                        </div>
+                        <button onClick={addUser} type="button" class="btn btn-primary mt-2"> {editId ? "Edit ": "Add User" }</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+              </div>
+            </div>
+          
         </>
     );
 }
